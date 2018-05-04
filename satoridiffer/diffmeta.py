7e2023bbd36dfef944b5db3fe9187683d3853791
operatorequals <john.torakis@gmail.com>
2018-05-04 18:04:30 +0300

    Added metadata and 'tags' in differ

--- satoridiffer/diffmeta.py
+++ satoridiffer/diffmeta.py
@@ -6,15 +6,31 @@ class DiffMeta(dict):
 
 	def __init__(self, source_im, dest_im):
 		super(DiffMeta, self).__init__()
-		self['source'] = source_im.get_class("system", section=_META_SECTION)
-		self['destination'] = dest_im.get_class("system", section=_META_SECTION)
-		self['timestamp'] = time.time()
-		self['unixtime'] = "{date} {tz} sec".format(
+
+		try:
+			self['src'] = source_im.get_class("system", section=_META_SECTION)
+			self['src_uuid'] = source_im.get_class("uuid", section=_META_SECTION)
+		except:
+			pass
+
+		try:
+			self['dst'] = dest_im.get_class("system", section=_META_SECTION)
+			self['dst_uuid'] = dest_im.get_class("uuid", section=_META_SECTION)
+		except:
+			pass
+
+		try:
+			self['src_type'] = source_im.__name__
+		except AttributeError:
+			self['src_type'] = type(source_im).__name__
+
+		try:
+			self['dst_type'] = dest_im.__name__
+		except AttributeError:
+			self['dst_type'] = type(dest_im).__name__
+
+		self['tstamp'] = time.time()
+		self['date'] = "{date} {tz} sec".format(
 				date=time.ctime(),
 				tz=time.timezone,
 			)
-
-
-
-# a = DiffMeta('a','b')
-# print (a) 
